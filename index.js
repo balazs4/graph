0fcfb93060db696ecd0459e7cd59c5e6151ad152
balazs4 <balazs4web@gmail.com>
2021-02-27 11:06:08 +0100

    minimal poc

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
+const { graphql, buildSchema } = require('graphql');
+
+const schema = buildSchema(`type Query { hello: String }`);
+const root = { hello: () => `world` };
+const resolve = (request) => graphql(schema, request, root);
+
 const { createServer } = require('http');
+const { parse } = require('url');
 const { PORT = 3000 } = process.env;
 
-const json = async (request) => {
-  const string = await new Promise((resolve, reject) => {
+const fromurl = (req) => {
+  const url = parse(req.url);
+  if (url.query === null || url.query === undefined) return '';
+
+  const { query = '' } = url.query
+    .split('&')
+    .map((kv) => kv.split('='))
+    .reduce((params, [key, value]) => ({ ...params, [key]: value }), {});
+  return query;
+};
+
+const frombody = async (req) => {
+  return new Promise((resolve, reject) => {
     const raw = [];
-    request
+    req
       .on('data', (chunk) => raw.push(chunk))
       .on('end', () => resolve(Buffer.concat(raw).toString('utf-8')));
   });
-  return JSON.parse(string);
 };
 
 createServer(async (req, res) => {
-  switch (req.headers['content-type']) {
-    case 'application/json':
-      return res
-        .writeHead(200, { 'content-type': 'application/json' })
-        .end(JSON.stringify(await json(req)), 'utf-8');
-
-    default:
-      return res
-        .writeHead(400, { 'content-type': 'text/plain' })
-        .end(err.message, 'utf-8');
+  const request = req.method === 'GET' ? fromurl(req) : await frombody(req);
+  if (request === '') {
+    return res
+      .writeHead(400, { 'content-type': 'text/plain' })
+      .end('Bad request', 'utf-8');
   }
+  const result = await resolve(request);
+  return res
+    .writeHead(200, { 'content-type': 'application/json' })
+    .end(JSON.stringify(result), 'utf-8');
 }).listen(PORT, () => {
   console.log(`Server listening...${PORT}`);
 });
