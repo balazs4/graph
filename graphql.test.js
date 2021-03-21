e25073de011c33919bca176dcfc13027dc14567c
balazs4 <balazs4web@gmail.com>
2021-03-21 13:57:56 +0100

    TIL: null prototype is a thing
    but why? what is benefit of it?

--- graphql.test.js
+++ graphql.test.js
@@ -1,8 +1,12 @@
 const assert = require('assert');
 const graphql = require('./graphql');
 
+const data = (x) => {
+  return { data: Object.setPrototypeOf(x, null) };
+};
+
 test('{ hello } query should return { "hello": "world"} data', async () => {
-  const actual = JSON.parse(JSON.stringify(await graphql('{hello}')));
-  const expected = { data: { hello: 'world' } };
+  const actual = await graphql('{hello}');
+  const expected = data({ hello: 'world' });
   assert.deepStrictEqual(actual, expected);
 });
