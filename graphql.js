e112de98168591ec53a01dda480fd11d4dce0a24
balazs4 <balazs4@users.noreply.github.com>
2021-09-29 18:26:25 +0200

    yOLO cOMmit :sponge:

--- graphql.js
+++ graphql.js
@@ -1,5 +1,20 @@
 const { graphql, buildSchema } = require('graphql');
 
-const schema = buildSchema(`type Query { hello: String }`);
-const root = { hello: () => `world` };
-module.exports = (request) => graphql(schema, request, root);
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const resolver = {
+  hello: () => `world`,
+};
+
+module.exports = (request) => {
+  const args = {
+    schema: schema,
+    source: request,
+    rootValue: resolver,
+  };
+  return graphql(args);
+};
